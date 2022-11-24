b352ae8aa5944d092c21dc2a5318a4fa52d915e7
Julian Wening <julian.wening@brz.eu>
2022-11-24 10:39:37 +0100

    Create endpoint to get all winner sentences

---
+++ pages/api/sentences/winner.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { sentence } from "@/lib/index"
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  const sentences = await sentence.getWinners()
+  console.log("Requesting...")
+  res.status(200).json(sentences ?? [])
+}
