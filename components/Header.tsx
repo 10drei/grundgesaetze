4f055e7256a1b1c6696d8a61ba89efc036cdfa2a
Daniel Stahl <daniel.stahl@brz.eu>
2022-11-25 11:15:13 +0100

    change font

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+//website header
+import React from "react"
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Über das Projekt</h1>
+    </div>
+  )
+}
+
+export default Header
