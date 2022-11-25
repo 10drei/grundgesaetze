73febd5bb89930034dcfcdb6ebc6f0e36b4dcc74
Julian Wening <julian.wening@brz.eu>
2022-11-25 12:30:48 +0100

    Implement & style header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,23 @@
 //website header
 import React from "react"
+import Link from "next/link"
+import styles from "./Header.module.css"
+import Image from "next/image"
+import instagram from "../public/instagram.svg"
 
 function Header() {
   return (
-    <div className="header">
-      <h1>Über das Projekt</h1>
+    <div className={styles.Header}>
+      <Link className={styles.link} href={"https://studypilot.app"}>
+        Über das Projekt
+      </Link>
+      <Link href="https://www.instagram.com/grundgesaetze/" target="_blank">
+        <Image
+          className={styles.instagramIcon}
+          src={instagram}
+          alt="Instagram"
+        />
+      </Link>
     </div>
   )
 }
