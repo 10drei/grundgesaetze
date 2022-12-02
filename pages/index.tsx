6b1e0cd897fe5db9efd32e6494319d4f34ee31fa
Julian Wening <julian.wening@brz.eu>
2022-12-02 12:11:50 +0100

    Create footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,9 @@ import Header from "../components/Header"
 import { sentence, right } from "../lib/index"
 import Search from "../components/sections/Search"
 import WinnerSentences from "../components/sections/WinnerSentences"
+import Link from "next/link"
+import Image from "next/image"
+import Logo from "../public/10drei_Logo_transparent.svg"
 
 interface FullSentence extends Sentence {
   phrases: Phrase[]
@@ -56,6 +59,46 @@ export default function Home({
       </div>
 
       <WinnerSentences sentences={sentences} rights={rights} />
+
+      <div className={styles.Footer}>
+        <Image className={styles.image} src={Logo} alt="Logo" />
+        <div className={styles.footerContent}>
+          <div className={styles.about}>
+            <h3>Über 10drei</h3>
+            <p>
+              Unsere Vision ist eine Gesellschaft, in der die im Grundgesetz
+              enthaltenen Werte, wie Würde, Freiheit und Gleichheit Grundlage
+              der alltäglichen Entscheidungsfindung sind. Dadurch gestalten wir
+              eine Zukunft, deren Fundament eine stabile Demokratie mit mündigen
+              Bürgern ist. Eine Zukunft, deren kleinster und zugleich stärkster
+              Nenner die geteilten Werte des Grundgesetzes sind.
+            </p>
+          </div>
+          <div className={styles.aboutProject}>
+            <h3>Über grundgeSÄTZE</h3>
+            <p>
+              Die Grundrechte unseres Grundgesetzes werden zu #grundgeSÄTZE -
+              einfach für alle und online zugänglich. Bestehend aus
+              Eigenformulierungen des Grundgesetzes aus über 40 Schulen
+              Deutschlands.
+            </p>
+          </div>
+
+          <div className={styles.contactInformation}>
+            <Link href="mailto:hallo@10drei.org">hallo@10drei.org</Link>
+            <p>10drei e.V., c/o Eleven gGmbH | Praterinsel 4, 80538 München</p>
+          </div>
+
+          <div className={styles.links}>
+            <Link target="_blank" href="https://10drei.org/impressum">
+              Impressum
+            </Link>
+            <Link target="_blank" href="https://10drei.org/datenschutz">
+              Datenschutz
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
