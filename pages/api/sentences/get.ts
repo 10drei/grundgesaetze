caffe93a8d21290e6fc0429fa368a215ae84cab0
Julian Wening <julian.wening@brz.eu>
2022-12-02 11:10:07 +0100

    Directly call prisma

--- pages/api/sentences/get.ts
+++ pages/api/sentences/get.ts
@@ -1,10 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next"
-import { getAll } from "@/lib/models/sentence"
+import db from "@/lib/db"
 
 export default async function getSentences(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const sentences = await getAll()
+  // const sentences = await getAll()
+  const sentences = db.sentence.findMany()
   res.status(200).json(sentences ?? [])
 }
