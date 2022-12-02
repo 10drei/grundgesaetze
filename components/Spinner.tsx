19b25294a307088d5b55f4b6b8a8a3cc989528dd
Julian Wening <julian@studypilot.app>
2022-12-02 15:28:30 +0100

    Change spinner shape

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -7,9 +7,19 @@ type Props = {
 
 function Spinner({ className }: Props) {
   return (
-    <div className={classNames(styles.spinner, className)}>
-      <div className={styles.doubleBounce1}></div>
-      <div className={styles.doubleBounce2}></div>
+    <div className={classNames(styles.skFadingCircle, className)}>
+      <div className={classNames(styles.skCircle1, styles.skCircle)}></div>
+      <div className={classNames(styles.skCircle2, styles.skCircle)}></div>
+      <div className={classNames(styles.skCircle3, styles.skCircle)}></div>
+      <div className={classNames(styles.skCircle4, styles.skCircle)}></div>
+      <div className={classNames(styles.skCircle5, styles.skCircle)}></div>
+      <div className={classNames(styles.skCircle6, styles.skCircle)}></div>
+      <div className={classNames(styles.skCircle7, styles.skCircle)}></div>
+      <div className={classNames(styles.skCircle8, styles.skCircle)}></div>
+      <div className={classNames(styles.skCircle9, styles.skCircle)}></div>
+      <div className={classNames(styles.skCircle10, styles.skCircle)}></div>
+      <div className={classNames(styles.skCircle11, styles.skCircle)}></div>
+      <div className={classNames(styles.skCircle12, styles.skCircle)}></div>
     </div>
   )
 }
